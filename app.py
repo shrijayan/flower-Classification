96db34dc085aba742d3a220a61596138a4138acc
Shrijayan Rajendran <shrijayan@cpluz.com>
2023-07-10 20:27:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('/content/my_model2.hdf5')
+  model=tf.keras.models.load_model('/my_model2.hdf5')
   return model
 with st.spinner('Model is being loaded..'):
   model=load_model()
